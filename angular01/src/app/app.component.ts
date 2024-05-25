5821b2b649bfd60ab1f21a1f18c4d84f8a74858e
Dido2002 <didoguzelev@gmail.com>
2024-05-25 14:46:41 +0300

    Learning routing between different components

--- angular01/src/app/app.component.ts
+++ angular01/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomeComponent, AboutComponent, ContactComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular01';
