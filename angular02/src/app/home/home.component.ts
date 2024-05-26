56d343348d11662086cdee7757e67520ef9001f8
Dido2002 <didoguzelev@gmail.com>
2024-05-26 13:19:54 +0300

    Create API request to endpoint with dummy data. First we create sevice client service that will handle the http request then in the home component where we want to render the clients data HomeComponent sets up and fetches client data on initialization.Utilizes ClientService to make the HTTP request.Data Handling: Subscribes to the service method to get data and handles it appropriately.Conditional Rendering: Displays the data or a message based on the data received.

--- angular02/src/app/home/home.component.ts
+++ angular02/src/app/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  clients: any[] = [];
 
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.clientService.getClients().subscribe(
+      (data) => {
+        this.clients = data;
+      },
+      (error) => {
+        console.log('There was an error', error);
+      }
+    );
+  }
 }
