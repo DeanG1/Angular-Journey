d4b476f19c5f692460178551b7d7b35392000eec
Dido2002 <didoguzelev@gmail.com>
2024-06-02 17:15:38 +0300

    Added post request to dummy endpoint & created custom http-service because HttpClientModule is depricated because of the version difference

--- angular03/src/app/login/login.component.ts
+++ angular03/src/app/login/login.component.ts
@@ -3,8 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,11 +13,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private authService: AuthService) {}
 
-  onSubmit(loginForm: any) {
+  onSubmit(loginForm: NgForm) {
     const username = loginForm.value.username;
     const password = loginForm.value.password;
+    this.authService.login(username, password).then(
+      (response) => {
+        console.log('Login successful', response);
+        this.showAlert('Login successful!');
+      },
+      (err) => {
+        console.error('Login failed', err);
+        this.showAlert('Login failed!');
+      }
+    );
     console.log(username, password);
     this.showAlert(`Username: ${username}, Password: ${password}`);
     this.openDialog(username, password);
